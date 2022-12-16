cb0e15c6328fae991b8459ecfe6554b20ae6e770
조용일 <yedn1128@naver.com>
2022-12-16 18:17:47 +0900

    시프링 뷰트 시큐리티 3강 - 시큐리티 회원가입

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -4,11 +4,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity  // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨.
 public class SecurityConfig {
+
+    /*
+기존: WebSecurityConfigurerAdapter를 상속하고 configure매소드를 오버라이딩하여 설정하는 방법
+=> 현재: SecurityFilterChain을 리턴하는 메소드를 빈에 등록하는 방식(컴포넌트 방식으로 컨테이너가 관리)
+//https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+
+@Override
+protected void configure(HttpSecurity http) throws  Exception{
+http.csrf().disable();
+http.authorizeRequests()
+        .antMatchers("/user/**").authenticated()
+        .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+        .antMatchers("/admin").access("\"hasRole('ROLE_ADMIN')")
+        .anyRequest().permitAll();
+}
+
+*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,25 +43,15 @@ public class SecurityConfig {
                 .and()
                 // 권한 필요한 설정 일 경우 login 페이지로 강제 이동 시킴.
                 .formLogin()
-                .loginPage("/login");
+                .loginPage("/loginForm");
 
         return http.build();
     }
 
-        /*
-    기존: WebSecurityConfigurerAdapter를 상속하고 configure매소드를 오버라이딩하여 설정하는 방법
-    => 현재: SecurityFilterChain을 리턴하는 메소드를 빈에 등록하는 방식(컴포넌트 방식으로 컨테이너가 관리)
-    //https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
-
-    @Override
-    protected void configure(HttpSecurity http) throws  Exception{
-        http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/user/**").authenticated()
-                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
-                .antMatchers("/admin").access("\"hasRole('ROLE_ADMIN')")
-                .anyRequest().permitAll();
+    @Bean
+    public BCryptPasswordEncoder encodePwd(){
+        // 프레임워크에서 제공하는 클래스 중 하나로 비밀번호를 암호화하는 데 사용할 수 있는 메서드를 가진 클래스.
+        return new BCryptPasswordEncoder();
     }
 
-     */
 }
--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,12 +1,21 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.domain.User;
+import com.cos.security1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final UserRepository userRepo;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     @GetMapping({"","/"})
     public String index(){
         // View는 머스테치 사용
@@ -29,18 +38,31 @@ public class IndexController {
     }
 
     // 스프링시큐리티가 해당 주소를 낚아챔.
-    @GetMapping("/login")
-    public @ResponseBody String login(){
-        return "login";
+//    @GetMapping("/login")
+//    public String login(){
+//        return "loginForm";
+//    }
+
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
     }
 
-    @GetMapping("/join")
-    public @ResponseBody String join(){
-        return "join";
+    @GetMapping("/joinForm")
+    public String JoinForm(){
+        return "joinForm";
     }
 
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc(){
-        return "회원가입 완료됨.";
+    @PostMapping("/join")
+    public String join(User user){
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        bCryptPasswordEncoder.match
+        userRepo.save(user);
+        return "redirect:/loginForm";
     }
+
 }
