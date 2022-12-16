0cd24f229d22dc5dd490a3089c08bcdf593adb1a
조용일 <yedn1128@naver.com>
2022-12-16 17:43:45 +0900

    스프링부트 시큐리티 2강 - 시큐리티 설정

---
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity  // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨.
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                // /user/** 들어오면 인증이 필요하다는 선언
+                .antMatchers("/user/**").authenticated()
+                // /manager/** login은 했지만 manager나 admin인 권한 만 접속 가능
+                .antMatchers("/manager/**").access("hasAnyRole('ROLE_MANAGER','ROLE_ADMIN')")
+                // /admin/** admin인 권한 만 접속 가능
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                // 저 위에 선언한 것을 제외한 모든 권한 허용, /login uri 시큐리티 로그인 페이지로 낚아채지던 현상 없애 버림.
+                .anyRequest().permitAll()
+                .and()
+                // 권한 필요한 설정 일 경우 login 페이지로 강제 이동 시킴.
+                .formLogin()
+                .loginPage("/login");
+
+        return http.build();
+    }
+
+        /*
+    기존: WebSecurityConfigurerAdapter를 상속하고 configure매소드를 오버라이딩하여 설정하는 방법
+    => 현재: SecurityFilterChain을 리턴하는 메소드를 빈에 등록하는 방식(컴포넌트 방식으로 컨테이너가 관리)
+    //https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+
+    @Override
+    protected void configure(HttpSecurity http) throws  Exception{
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin").access("\"hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+    }
+
+     */
+}
