019ae0fb68f4934f6aaf81dff195350fe6858011
조용일 <yedn1128@naver.com>
2022-12-28 16:34:52 +0900

    스프링부트 시큐리티 11강 - 네이버 로그인 완료

--- src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -4,6 +4,7 @@ import com.cos.security1.config.app.CustomBCryptPasswordEncoder;
 import com.cos.security1.config.auth.PrincipalDetails;
 import com.cos.security1.config.auth.provider.FacebookUserInfo;
 import com.cos.security1.config.auth.provider.GoogleUserInfo;
+import com.cos.security1.config.auth.provider.NaverUserInfo;
 import com.cos.security1.config.auth.provider.OAuth2UserInfo;
 import com.cos.security1.domain.User;
 import com.cos.security1.repository.UserRepository;
@@ -14,6 +15,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -43,8 +46,11 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
             System.out.println("페이스북 로그인 요청");
             oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            System.out.println("네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
         } else {
-            System.out.println("구글 페이스북만 지원");
+            System.out.println("구글 페이스북 네이버만 지원");
         }
 
         String provider = oAuth2UserInfo.getProvider();
